b1691ff6755bfa09287bf804645aa3added25d14
Albert Dong <albertd@ucla.edu>
2023-08-24 10:15:29 -0700

    C++ DFS optimizations

--- solver.cpp
+++ solver.cpp
@@ -7,11 +7,11 @@
 #include <vector>
 using namespace std;
 
-string calc_hash(vector<vector<int>> &grid) {
-    string output = "";
-    for (vector<int> row : grid) {
+long long calc_hash(array<array<int, 17>, 10> &grid) {
+    long long output = 0;
+    for (array<int, 17> row : grid) {
         for (int num : row) {
-            output += to_string(num);
+            output = output * 33 + num + 1;
         }
     }
 
@@ -21,15 +21,15 @@ string calc_hash(vector<vector<int>> &grid) {
 int explored = 0;
 int last = 0;
 auto start = chrono::steady_clock::now();
-void dfs(pair<vector<vector<int>>, int> &best, vector<vector<int>> &grid, int score,
-         vector<vector<int>> &path, unordered_set<string> &visit) {
+void dfs(array<array<int, 17>, 10> grid, int score, vector<vector<int>> &path,
+         unordered_set<long long> &visit, pair<vector<vector<int>>, int> &best) {
     visit.insert(calc_hash(grid));
     if (score > best.second) {
         cout << "new solution: " << score << endl;
         best = make_pair(path, score);
     }
     explored++;
-    if (explored == last + 1000) {
+    if (explored == last + 10000) {
         cout << explored << endl;
         last = explored;
     }
@@ -39,71 +39,73 @@ void dfs(pair<vector<vector<int>>, int> &best, vector<vector<int>> &grid, int sc
 
         cout << elapsed.count() << endl;
     }
-    int sum = 0;
-    int apples = 0;
+
+    int prefix[10][18][2];
     for (int r = 0; r < 10; r++) {
-        for (int c = 0; c < 17 && count(grid[r].begin(), grid[r].end(), 0) != 17; c++) {
-            for (int r_size = 1; r_size < 10 - r + 1; r_size++) {
-                for (int c_size = 1; c_size < 17 - c + 1; c_size++) {
-                    // Build submatrix of interest
-                    vector<vector<int>> submatrix;
-                    sum = 0;
-                    apples = 0;
-                    for (int sub_r = 0; sub_r < r_size; sub_r++) {
-                        submatrix.push_back(vector<int>(grid[r + sub_r].begin() + c,
-                                                        grid[r + sub_r].begin() + c + c_size));
-                        sum += accumulate(submatrix[sub_r].begin(), submatrix[sub_r].end(), 0);
-                        apples +=
-                            c_size - count(submatrix[sub_r].begin(), submatrix[sub_r].end(), 0);
-                        if (sum == 0 || sum > 10) {
+        prefix[r][0][0] = 0;
+        prefix[r][0][1] = 0;
+        for (int c = 1; c <= 17; c++) {
+            prefix[r][c][0] = prefix[r][c - 1][0] + grid[r][c - 1];
+            prefix[r][c][1] = prefix[r][c - 1][1] + (grid[r][c - 1] != 0 ? 1 : 0);
+        }
+    }
+    int target_apples = 2;
+    int branches = 0;
+    while (target_apples <= 10 && branches < 3) {
+        for (int r = 0; r < 10 && branches < 3; r++) {
+            for (int c = 0; c < 17 && branches < 3; c++) {
+                for (int r_size = 1; r_size < 10 - r + 1 && branches < 3; r_size++) {
+                    for (int c_size = 1; c_size < 17 - c + 1 && branches < 3; c_size++) {
+                        int sum = 0;
+                        int apples = 0;
+                        for (int sub_r = 0; sub_r < r_size; sub_r++) {
+                            sum += prefix[r + sub_r][c + c_size][0] - prefix[r + sub_r][c][0];
+                            apples += prefix[r + sub_r][c + c_size][1] - prefix[r + sub_r][c][1];
+                        }
+
+                        if (sum > 10 || apples > target_apples)
+                            break;
+                        else if (sum == 10 && apples == target_apples) {
+                            auto copy_grid = grid;
+                            for (int sub_r = 0; sub_r < r_size; sub_r++) {
+                                for (int sub_c = 0; sub_c < c_size; sub_c++) {
+                                    copy_grid[r + sub_r][c + sub_c] = 0;
+                                }
+                            }
+
+                            if (visit.find(calc_hash(copy_grid)) == visit.end()) {
+                                branches++;
+                                vector<int> action = {r, c, r_size, c_size};
+                                path.push_back(action);
+                                dfs(copy_grid, score + apples, path, visit, best);
+                                path.pop_back();
+                            }
+
                             break;
                         }
-                    }
-
-                    if (sum > 10 || (score < 20 && apples > 2))
-                        break;
-                    else if (sum == 10) {
-                        for (int sub_r = 0; sub_r < r_size; sub_r++) {
-                            for (int sub_c = 0; sub_c < c_size; sub_c++) {
-                                grid[r + sub_r][c + sub_c] = 0;
-                            }
-                        }
-
-                        if (visit.find(calc_hash(grid)) == visit.end()) {
-                            vector<int> action = {r, c, r_size, c_size};
-                            path.push_back(action);
-                            dfs(best, grid, score + apples, path, visit);
-                            path.pop_back();
-                        }
-
-                        for (int sub_r = 0; sub_r < r_size; sub_r++) {
-                            for (int sub_c = 0; sub_c < c_size; sub_c++) {
-                                grid[r + sub_r][c + sub_c] = submatrix[sub_r][sub_c];
-                            }
-                        }
-
-                        break;
                     }
                 }
             }
         }
+
+        target_apples++;
     }
 }
 
 int main() {
     pair<vector<vector<int>>, int> best = make_pair(vector<vector<int>>(), 0);
-    unordered_set<string> visit;
+    unordered_set<long long> visit;
     vector<vector<int>> path;
-    vector<vector<int>> grid;
-
+    array<array<int, 17>, 10> grid;
     srand(time(0));
     for (int i = 0; i < 10; i++) {
-        vector<int> a;
         for (int j = 0; j < 17; j++) {
-            a.push_back((rand() % 9) + 1);
+            grid[i][j] = (rand() % 9) + 1;
         }
-        grid.push_back(a);
     }
 
-    dfs(best, grid, 0, path, visit);
+    // for (int r = 0; r < 10; r++)
+    //     for (int c = 0; c < 17; c++) cin >> grid[r][c];
+
+    dfs(grid, 0, path, visit, best);
 }
--- solver.py
+++ solver.py
@@ -22,26 +22,38 @@ def find_solution(grid: np.array):
             print(f"new solution: {score}")
             best = [path[:], score]
 
-        for r in range(10):
-            for c in range(17):
-                for r_size in range(1, 10 - r + 1):
-                    for c_size in range(1, 17 - c + 1):
-                        section = grid[r : r + r_size, c : c + c_size]
+        target_apples = 2
+        discovered = False
+        branches = 0
+        while not discovered and target_apples <= 10:
+            for r in range(10):
+                for c in range(17):
+                    for r_size in range(1, 10 - r + 1):
+                        for c_size in range(1, 17 - c + 1):
+                            if branches < 4:
+                                section = grid[r : r + r_size, c : c + c_size]
 
-                        if np.count_nonzero(section) > 10 or np.sum(section) > 10:
-                            break
-                        elif np.sum(section) == 10:
-                            old_section = section.copy()
-                            section.fill(0)
-                            if grid.tobytes() not in visit:
-                                path.append((r, c, r_size, c_size))
-                                dfs(
-                                    grid=grid,
-                                    score=score + np.count_nonzero(old_section),
-                                )
-                                path.pop()
-                            grid[r : r + r_size, c : c + c_size] = old_section
-                            break
+                                if (
+                                    np.count_nonzero(section) > target_apples
+                                    or np.sum(section) > 10
+                                ):
+                                    break
+                                elif np.sum(section) == 10:
+                                    old_section = section.copy()
+                                    section.fill(0)
+                                    if grid.tobytes() not in visit:
+                                        discovered = True
+                                        branches += 1
+                                        path.append((r, c, r_size, c_size))
+                                        dfs(
+                                            grid=grid,
+                                            score=score + np.count_nonzero(old_section),
+                                        )
+                                        path.pop()
+                                    grid[r : r + r_size, c : c + c_size] = old_section
+                                    break
+            if not discovered:
+                target_apples += 1
 
     dfs(grid, 0)
     print(f"calculated optimal score: {best[1]}")
